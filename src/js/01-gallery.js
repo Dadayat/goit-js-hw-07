6eefa4bdada63329d0347eae0adb21d81d1d2a6d
Dadayat <136356333+Dadayat@users.noreply.github.com>
2023-10-15 19:36:48 +0300

    1-2task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -24,41 +24,6 @@ function createGalleryItemsMarkup(items) {
     .join('');
 }
 
-// create modal
-// function onImgClick(e) {
-//   e.preventDefault();
-//   // if (e.target.nodeName !== "IMG") return;
-
-//   const isItemImage = e.target.classList.contains('gallery__image');
-//   if (!isItemImage) return;
-
-//   const currentImgUrl = e.target.dataset.source;
-
-//   const instance = basicLightbox.create(
-//     `
-// 		<img src="${currentImgUrl}" width="1280" height="auto"/>
-//         `,
-//     {
-//       onShow: (instance) => {
-//         window.addEventListener('keydown', onEscKeyPress);
-//       },
-//       onClose: (instance) => {
-//         window.removeEventListener('keydown', onEscKeyPress);
-//       },
-//     }
-//   );
-//   instance.show();
-
-//   function onEscKeyPress(e) {
-//     const ESC_KEY_CODE = 'Escape';
-//     const isEscKey = e.code === ESC_KEY_CODE;
-//     if (!isEscKey) return;
-//     instance.close();
-//   }
-// }
-
-// ------- 2nd variant --------------------------------------------------------------------------------
-
 const instance = basicLightbox.create(
   `
 <img width="1280" height="auto" src="">`,
